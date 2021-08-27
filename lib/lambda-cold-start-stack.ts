a73ac028519361f4d29d3204d1bd73a6305f145c
Paul O'Fallon <pofallon@users.noreply.github.com>
2021-08-27 10:36:00 -0400

    Add python & lambda insights

--- lib/lambda-cold-start-stack.ts
+++ lib/lambda-cold-start-stack.ts
@@ -2,10 +2,14 @@ import * as path from 'path';
 import * as cdk from '@aws-cdk/core';
 import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
 import { RetentionDays } from '@aws-cdk/aws-logs';
-import { DockerImageFunction, DockerImageCode } from '@aws-cdk/aws-lambda';
+import { DockerImageFunction, DockerImageCode, Runtime, LayerVersion } from '@aws-cdk/aws-lambda';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python';
 import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
 import { SfnExecFunction } from './sfn-exec-function';
 import { LoadTester } from './load-tester';
+import { ManagedPolicy } from '@aws-cdk/aws-iam';
+
+const insightsLayerArn = `arn:aws:lambda:${process.env.CDK_DEFAULT_REGION}:580247275435:layer:LambdaInsightsExtension:14`;
 
 export class LambdaColdStartStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -18,11 +22,26 @@ export class LambdaColdStartStack extends cdk.Stack {
       logRetention: RetentionDays.ONE_WEEK,
       timeout: cdk.Duration.seconds(60)
     });
+    dotNet5Function.role?.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLambdaInsightsExecutionRolePolicy'))
     const dotNet5Integration = new LambdaProxyIntegration({ handler: dotNet5Function });
     httpApi.addRoutes({ path: '/dotnet5-container', methods: [ HttpMethod.GET ], integration: dotNet5Integration });
 
+    const python3Function = new PythonFunction(this, 'Python3Function', {
+      entry: path.join(__dirname, '..', 'src', 'python3'),  
+      index: 'function.py',
+      handler: 'lambda_handler',
+      runtime: Runtime.PYTHON_3_7,
+      layers: [ LayerVersion.fromLayerVersionArn(this, 'LayerFromArn', insightsLayerArn) ],
+      logRetention: RetentionDays.ONE_WEEK,
+      timeout: cdk.Duration.seconds(60)
+    });
+    python3Function.role?.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLambdaInsightsExecutionRolePolicy'))
+    
+    const python3Integration = new LambdaProxyIntegration({ handler: python3Function });
+    httpApi.addRoutes({ path: '/python3', methods: [ HttpMethod.GET ], integration: python3Integration })
+
     const loadTester = new LoadTester(this, 'LoadTester', {
-      routes: ['dotnet5-container'],
+      routes: ['dotnet5-container', 'python3'],
       prefix: httpApi.apiEndpoint
     });
 
--- lib/load-tester.request.ts
+++ lib/load-tester.request.ts
@@ -5,8 +5,9 @@ const routes = process.env.routes?.split(',');
 const handler = async (event: any) => {
 
   console.log(event);
+  console.log(routes);
   
-  const promises = routes?.map(r => got(r, {prefixUrl: process.env.prefix}) );
+  const promises = routes?.map(r => got(r, { prefixUrl: process.env.prefix }) );
   // @ts-ignore
   await Promise.all(promises);
 
